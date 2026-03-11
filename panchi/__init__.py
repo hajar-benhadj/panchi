1cc49f67e45f9193e43317128ae43a2edb25e6fb
gustavogalvao <gugagalvao@live.com>
2026-03-11 13:08:29 -0400

    Fixed version tag again 2

--- panchi/__init__.py
+++ panchi/__init__.py
@@ -5,7 +5,7 @@ panchi prioritizes clarity and understanding over performance, making it ideal
 for students, educators, and anyone who wants to see how linear algebra really works.
 """
 
-__version__ = "0.1.0a1"
+__version__ = "0.2.0a1"
 
 from panchi.primitives.vector import Vector
 from panchi.primitives.matrix import Matrix
